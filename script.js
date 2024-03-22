ca85a8c6acea20d39c3a47bc2d1a5a4cfbedc111
CJKbarts <bartelsjacobcorn@gmail.com>
2024-03-22 13:24:19 -0500

    Add clear button and createNewGrid button
    Clear allows the user to reset the grid with the same number of boxes
    Create new grid allows the user to reset the grid with a different
    number of buttons

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const gridContainer = document.querySelector(".gridContainer");
-let numOfBoxes = 16;
+let numOfBoxesValue = 16;
+
+const createNewGridBtn = document.querySelector(".createNewGridBtn");
+createNewGridBtn.addEventListener("click", ()=> {
+    numOfBoxesValue = prompt("Enter the number of boxes wanted (Max:100)");
+    resetGrid(numOfBoxesValue);
+})
+
+const clearBtn = document.querySelector(".clearBtn");
+clearBtn.addEventListener("click", ()=> resetGrid());
+
+function resetGrid(numOfBoxes = numOfBoxesValue){
+    deleteGrid();
+    createGrid(numOfBoxes);
+}
 
 function createGrid(numOfBoxes){
     for(let i = 0; i < numOfBoxes; i++){
@@ -24,8 +38,12 @@ function createBox(){
     return box;    
 }
 
+function deleteGrid(){
+    gridContainer.innerHTML = "";
+}
+
 function colorBox(e){
     e.target.classList.add("colored");
 }
 
-createGrid(numOfBoxes);
+createGrid(numOfBoxesValue);
